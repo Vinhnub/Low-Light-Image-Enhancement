0069fbe8a4ae639d5274aa81dea4f530a2b0611a
huyhd2334 <huyqame1356@gmail.com>
2026-07-05 21:29:58 +0700

    frontend function basic

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "26.198.149.7",
+  ],
 };
 
 export default nextConfig;
